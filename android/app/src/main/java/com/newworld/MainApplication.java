06cc39902b21b1c1f0240180544ba4ab75a17f9a
jiangtt18 <tjiang90111@gmail.com>
2018-05-24 00:18:08 -0700

    fixed all installation bugs

--- android/app/src/main/java/com/newworld/MainApplication.java
+++ android/app/src/main/java/com/newworld/MainApplication.java
@@ -23,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new MapsPackage()
       );
     }
 
@@ -44,11 +45,5 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage */ false);
   }
 
-  @Override
-     protected List<ReactPackage> getPackages() {
-         return Arrays.<ReactPackage>asList(
-                 new MainReactPackage(),
-                 new MapsPackage()
-         );
-     }
+
 }
