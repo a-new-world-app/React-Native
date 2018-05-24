dc34f16e80fcb1ad3a5df325142b605ab59b59ce
jiangtt18 <tjiang90111@gmail.com>
2018-05-24 01:17:01 -0700

    can show marker

--- App.js
+++ App.js
@@ -13,22 +13,13 @@ import {
 } from 'react-native';
 import MapView from 'react-native-maps';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-
         <MapView
           style={styles.map}
            initialRegion={{
@@ -36,8 +27,14 @@ export default class App extends Component<Props> {
              longitude: -122.4324,
              latitudeDelta: 0.0922,
              longitudeDelta: 0.0421,
-           }}
-         />
+           }}>
+           <MapView.Marker
+              coordinate={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+              }}
+            />
+         </MapView>
       </View>
     );
   }
