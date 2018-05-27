9501e8fa8db9d38b53331a4923da5e92d4c7fa8d
jiangtt18 <tjiang90111@gmail.com>
2018-05-27 00:31:45 -0700

    added reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,3 +1,14 @@
-export default (state = {}, action) => {
-  return state;
+// export default (state = {}, action) => {
+//   return state;
+// }
+
+import { FETCH_USER } from '../actions/Description';
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
+    default:
+    return state;
+  }
 }
