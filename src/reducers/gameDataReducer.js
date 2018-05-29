d61f1febbe55602bb6102c475364f18a8d7f1372
JonHalloran <angmar139@hotmail.com>
2018-05-29 11:50:57 -0700

    Pre debugging gamelogic

--- src/reducers/gameDataReducer.js
+++ src/reducers/gameDataReducer.js
@@ -1,13 +1,20 @@
+import { merge } from 'lodash';
+
 import { RECEIVE_GAME_DATA } from "../actions/gameDataActions";
+import { calculateProgress } from '../utils/gameDataUtil';
 
 const gameDataReducer = (oldState = {}, action) => {
   Object.freeze(oldState);
+  let newState = {};
   switch (action.type) {
     case RECEIVE_GAME_DATA:
-      return action.payload.gameData;
+      newState = merge(newState, oldState, action.payload.gameData);
+      break;
     default:
-      return oldState;
+      newState = merge(newState, oldState);
   }
+  newState = calculateProgress(newState);
+  return newState;
 };
 
 export default gameDataReducer;
--- src/utils/gameDataUtil.js
+++ src/utils/gameDataUtil.js
@@ -1,54 +1,71 @@
-import robotTypes from '../../assets/robots/robotTypes.js'
+import robotTypes from '../../assets/robots/robotTypes.js';
 
 export const calculateProgress = (gameData) => {
-  const taskArr = gameData += 1;
+  const resourceTypes = [
+    'iron',
+    'copper',
+    'aluminum',
+    'gold',
+    'titanium',
+  ];
+  const taskArr = gameData.tasks;
   const now = Date.now();
-  const robotWork = RobotWork(gameData.robots)
-  let addedRes = handleExplore(gameData.explore, robotWork.explore, now)
+  const robotWorkStrength = robotWork(gameData.robots);
+  let addedRes = handleExplore(gameData.explore, robotWorkStrength.explore, now);
   for (let task in taskArr) {
     if (task.type === 'BUILD') {
       task.progress += //TODO build
       task.lastUpdated = now;
       if (task.progress > task.required){
         gameData.robots[task.building] += 1;
+        task = 'COMPLETE';
       }
-      task = 'COMPLETE';
-    } else if (task.type === 'Gather') {
-      
+    } else if (task.type === 'GATHER') {
+      if (now > task.end){
+        addedRes[task.resource] += task.amount;
+        gameData.robots[task.robot] += 1;
+        task = 'COMPLETE';
+      }
     }
   }
+  for (let resource in resourceTypes){
+    gameData.resources[resource] += addedRes[resource];
+  }
+
+  const ongoingTasks = taskArr.map((task)=> Array.isArray(task));
+  gameData.tasks = ongoingTasks;
+  return gameData;
 };
 
-private
 
-const robotWork = (robots) => {
+function robotWork(robots) {
   const work = {
     build: 0,
     explore: 0
-  }
+  };
   Object.keys(robots).forEach((robotKey) =>{ 
-    let robot = robots[robotKey]
-    work.build += robot.build * robotTypes.build
-    work.explore += robot.explore * robotTypes.explore
-  })
+    let robot = robots[robotKey];
+    work.build += robot.build * robotTypes.build;
+    work.explore += robot.explore * robotTypes.explore;
+  });
 
   return work;
 }
 
-const handleExplore = (exploring, exploringStrength, now) => {
-  resources = {
+function handleExplore(exploring, exploringStrength, now) {
+  let resources = {
     iron: 0,
     copper: 0,
     aluminum: 0,
     gold: 0,
     titanium: 0
-  }
+  };
   if (exploring.lastCheck + 1000 * 60 * 10 < now){
-    resources.iron += Math.ceil((exploringStrength * 10 * Math.random())/(60000(now - exploring.lastCheck)))
-    resources.iron += Math.ceil((exploringStrength * 5 * Math.random())/(60000(now - exploring.lastCheck)))
-    resources.iron += Math.ceil((exploringStrength * 3 * Math.random())/(60000(now - exploring.lastCheck)))
-    resources.iron += Math.ceil((exploringStrength * 2 * Math.random())/(60000(now - exploring.lastCheck)))
-    resources.iron += Math.ceil((exploringStrength * 0.1 * Math.random())/(60000(now - exploring.lastCheck)))
+    resources.iron += Math.ceil((exploringStrength * 10 * Math.random())/(60000(now - exploring.lastCheck)));
+    resources.iron += Math.ceil((exploringStrength * 5 * Math.random())/(60000(now - exploring.lastCheck)));
+    resources.iron += Math.ceil((exploringStrength * 3 * Math.random())/(60000(now - exploring.lastCheck)));
+    resources.iron += Math.ceil((exploringStrength * 2 * Math.random())/(60000(now - exploring.lastCheck)));
+    resources.iron += Math.ceil((exploringStrength * 0.1 * Math.random())/(60000(now - exploring.lastCheck)));
     exploring.lastCheck = now;
   }
 
