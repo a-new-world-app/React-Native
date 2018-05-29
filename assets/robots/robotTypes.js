310398ed29de87f41764a978078e3f1b9849bd40
JonHalloran <angmar139@hotmail.com>
2018-05-29 12:05:10 -0700

    Add robot types

---
+++ assets/robots/robotTypes.js
@@ -0,0 +1,43 @@
+const robotTypes = {
+  1: {
+    pic: require('./bob.png'),
+    name: "Bob",
+    build: 1,
+    explore: 1,
+    carry: 1
+  },
+
+  2: {
+    pic: require('./drew.png'),
+    name: "Drew",
+    build: 5,
+    explore: 0,
+    carry: 0
+  },
+
+  3: {
+    pic: require('./leslie.png'),
+    name: "Leslie",
+    build: 1,
+    explore: 1,
+    carry: 5
+  },
+
+  4: {
+    pic: require('./shannon.png'),
+    name: "Shannon",
+    build: 3,
+    explore: 3,
+    carry: 3
+  },
+
+  5: {
+    pic: require('./spark.png'),
+    name: "Spark",
+    build: 0,
+    explore: 5,
+    carry: 1
+  }
+};
+
+export default robotTypes;
