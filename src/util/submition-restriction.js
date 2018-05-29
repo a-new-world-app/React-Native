d1ba68b2bd335adc4673d4e73141e7dd623b353e
jiangtt18 <tjiang90111@gmail.com>
2018-05-29 15:35:32 -0700

    add submit validation;

---
+++ src/util/submition-restriction.js
@@ -0,0 +1,27 @@
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180);
+}
+
+function isDescriptionValid(description) {
+  let length = description.length;
+  return length > 10 ; // need to have at least 10 characters to describe the path
+}
+
+function isCloseToLandmark(lat1,lon1,lat2,lon2){
+  let distanceInKm = getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2);
+  return distanceInKm < 0.05; // need to be less than 50 meters away from landmark
+}
